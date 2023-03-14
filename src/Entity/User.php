97e06d7ad888f99fdbf3917604e5d4f52f3a8677
fatihavci06 <66fatihavci@gmail.com>
2023-03-14 09:40:48 +0300

    s

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,137 +1,115 @@
 <?php
-
+ 
 namespace App\Entity;
-
+ 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-
-/**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- */
-class User
+ 
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $firstName;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $lastName;
-
-    /**
-     * @ORM\Column(type="string", length=25, unique=true)
-     */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
+ 
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
+ 
+    #[ORM\Column(type: 'string', length: 180)]
+    private $username;
+ 
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+ 
+    #[ORM\Column(type: 'string')]
     private $password;
-
+ 
+     
+ 
     public function getId(): ?int
     {
         return $this->id;
     }
-
-    /**
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    public function setUsername($username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
-
+ 
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
+ 
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
+ 
         return $this;
     }
-
+ 
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+ 
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+ 
+        return $this;
+    }
+ 
     /**
-     * @return string|null
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
      */
-    public function getPassword()
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+ 
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+ 
+        return array_unique($roles);
+    }
+ 
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+ 
+        return $this;
+    }
+ 
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
-
-    /**
-     * @param $password
-     */
-    public function setPassword($password)
+ 
+    public function setPassword(string $password): self
     {
         $this->password = $password;
+ 
+        return $this;
     }
-
+ 
     /**
-     * @return string|null
+     * @see UserInterface
      */
-    public function getSalt()
-    {
-        return null;
-    }
-
-    /**
-     * @return array|string[]
-     */
-    public function getRoles()
-    {
-        return array('ROLE_USER');
-    }
-
     public function eraseCredentials()
     {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
+ 
+    
 }
